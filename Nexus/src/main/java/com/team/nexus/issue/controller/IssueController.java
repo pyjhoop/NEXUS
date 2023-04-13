60dae28f66d9e4e0c69f1f36dd2b8b13ee6f4e5e
biho <mminimi27@gmail.com>
2023-04-14 03:49:08 +0900

    종모양 실시간 알림

--- Nexus/src/main/java/com/team/nexus/issue/controller/IssueController.java
+++ Nexus/src/main/java/com/team/nexus/issue/controller/IssueController.java
@@ -14,25 +14,20 @@ public class IssueController {
 
 	@Autowired
 	private IssueServiceImpl iService;
-	
-	
+
 	@RequestMapping("issueShow.mini")
 	public String issueList() {
 		return "issue/issueList";
 	}
-	
-	
+
 	@RequestMapping("issueEnroll.mini")
 	public String issueEnrollForm() {
 		return "issue/issueEnrollView";
 	}
-	
-	
-	
+
 	@RequestMapping("issueDetail.mini")
 	public String selectIssue(int ino, Model model) {
-		
-		
+
 //			Issue is = iService.selectIssue(ino);
 //			
 //			if( is != null) {
@@ -43,27 +38,16 @@ public class IssueController {
 //				return "common/errorPage";
 //				
 //			}
-		
 
 		return "issue/issueDetail";
-		
+
 	}
-	
-	
-	
+
 	@RequestMapping("issueDelete.mini")
 	public String deleteIssue(HttpSession session) {
 		session.setAttribute("alertMsg", "성공적으로 이슈가 삭제되었습니다");
-		
+
 		return "redirect:issueShow.mini";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
